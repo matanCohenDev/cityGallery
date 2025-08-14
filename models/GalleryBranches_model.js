7f9e203712178aa341d8f64ff370633b2057c615
Student D <shoam1508@walla.co>
2025-08-14 12:36:04 +0300

    Feat: models/GalleryBranches_model.js

--- models/GalleryBranches_model.js
+++ models/GalleryBranches_model.js
@@ -1,28 +1,12 @@
 const mongoose = require('mongoose');
 
 const branchSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    coordinates: {
-        lat: {
-            type: Number,
-            required: true,
-        },
-        lng: {
-            type: Number,
-            required: true,
-        },
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-});
+  name:    { type: String, required: true, trim: true },
+  address: { type: String, required: true, trim: true },
+  coordinates: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Branch', branchSchema);
